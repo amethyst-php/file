2e2055c000f62d07fc0e2b2da84147a5eb823678
railken <railken@protonmail.ch>
2018-09-09 23:16:42 +0200

    fix list managers

--- src/FileServiceProvider.php
+++ src/FileServiceProvider.php
@@ -21,7 +21,7 @@ class FileServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->loadRoutes();
 
-        config(['ore.permission.managers' => array_merge(Config::get('ore.permission.managers', []), [
+        config(['ore.managers' => array_merge(Config::get('ore.managers', []), [
             \Railken\LaraOre\File\FileManager::class,
         ])]);
     }
