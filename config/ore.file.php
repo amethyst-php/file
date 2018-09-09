544f588834bba2650308aa0e03198fab9426411c
railken <railken@protonmail.ch>
2018-09-09 23:16:34 +0200

    fix prefix api

--- config/ore.file.php
+++ config/ore.file.php
@@ -107,7 +107,7 @@ return [
             'enabled'    => true,
             'controller' => Railken\LaraOre\Http\Controllers\Admin\FilesController::class,
             'router'     => [
-                'prefix'      => '/admin/files',
+                'prefix'      => '/files',
             ],
         ],
     ],
