1c87df0308565374b73c3994d89fb4e8d017ebe6
railken <railken@protonmail.ch>
2019-05-23 22:41:33 +0200

    fix style

--- src/ApacheMimeTypes/PhpRepository.php
+++ src/ApacheMimeTypes/PhpRepository.php
@@ -4,10 +4,10 @@ namespace Railken\Amethyst\ApacheMimeTypes;
 
 class PhpRepository extends \Dflydev\ApacheMimeTypes\PhpRepository
 {
-	public function addExtension(string $key, array $value)
-	{
-		$currentExtensions = isset($this->typeToExtensions[$key]) ? $this->typeToExtensions[$key] : [];
+    public function addExtension(string $key, array $value)
+    {
+        $currentExtensions = isset($this->typeToExtensions[$key]) ? $this->typeToExtensions[$key] : [];
 
-		$this->typeToExtensions[$key] = array_merge($value, $currentExtensions);
-	}
+        $this->typeToExtensions[$key] = array_merge($value, $currentExtensions);
+    }
 }
