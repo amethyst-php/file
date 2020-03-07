7dcd221b75e494d580dcbfb593a9b2812231e5f4
Railken <railken@protonmail.ch>
2020-03-07 11:44:29 +0100

    tmp disable extra routes

--- src/Providers/FileServiceProvider.php
+++ src/Providers/FileServiceProvider.php
@@ -16,10 +16,12 @@ class FileServiceProvider extends CommonServiceProvider
 
         $this->app->register(\Spatie\MediaLibrary\MediaLibraryServiceProvider::class);
 
+        /*
         app('amethyst.router')->routes('file', function ($router) use ($config) {
             $controller = FilesController::class;
             $router->post('/{id}/upload', ['as' => 'upload', 'uses' => $controller.'@upload']);
             $router->post('/{id}/download', ['as' => 'download', 'uses' => $controller.'@download']);
         });
+        */
     }
 }
